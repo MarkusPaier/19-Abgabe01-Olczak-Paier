a52d3cb1d149a27a9def31d60e5fa05ed0f6cc42
Markus Paier <markus.paier@edu.fh-joanneum.at>
2019-03-27 14:46:03 +0100

    added sum() method with while-loop

--- GitCalculatorExercise/src/main/java/Calculator.java
+++ GitCalculatorExercise/src/main/java/Calculator.java
@@ -66,4 +66,6 @@ public interface Calculator {
 
     void addValue(int value);
 
+    int sum();
+
 }
--- GitCalculatorExercise/src/main/java/Main.java
+++ GitCalculatorExercise/src/main/java/Main.java
@@ -31,6 +31,16 @@ public class Main {
                 list.add(value);
             }
 
+            public int sum() {
+                int index = 0;
+                int result = 0;
+                while(index < list.size()) {
+                    result += list.get(index);
+                    index++;
+                }
+
+                return result;
+            }
         };
 
         calc.addValue(20);    // currently highest number
@@ -49,5 +59,6 @@ public class Main {
         System.out.println("highest number: " + calc.getMaximum());
 
         // output sum of all numbers
+        System.out.print("sum of all numbers: " + calc.sum());
     }
 }
